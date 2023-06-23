e01e1731d9c075b16da5ca2a5b64ef5e11a2e8f3
Tom Béné <himself@fantomebeig.net>
2023-06-23 22:14:21 +0200

    script to setup db index

---
+++ scripts/setup_meili.js
@@ -0,0 +1,32 @@
+import { MeiliSearch } from 'meilisearch';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new MeiliSearch({
+	host: process.env.PUBLIC_MEILI_HOST,
+	apiKey: process.env.MEILI_MASTER_KEY
+});
+
+client.createIndex('spinners', { primaryKey: 'id' });
+client.index('spinners').updateFilterableAttributes(['board']);
+client
+	.index('spinners')
+	.updateSearchableAttributes(['name', 'aliases', 'twitter_name', 'youtube_name']);
+client
+	.index('spinners')
+	.updateDisplayedAttributes([
+		'name',
+		'aliases',
+		'twitter_name',
+		'twitter_link',
+		'youtube_name',
+		'youtube_link'
+	]);
+client.index('spinners').updateDistinctAttribute('name');
+client.index('spinners').updateTypoTolerance({
+	minWordSizeForTypos: {
+		oneTypo: 2,
+		twoTypos: 4
+	}
+});
