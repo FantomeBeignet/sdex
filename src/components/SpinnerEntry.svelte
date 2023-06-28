dfb3038602e76281ee2848e57239238b4a021aa9
Tom Béné <himself@fantomebeig.net>
2023-06-28 23:20:17 +0200

    useless

--- src/components/SpinnerEntry.svelte
+++
@@ -1,8 +0,0 @@
-<<script lang='ts'>
-	export let name: string
-	export let twitter_name: string
-	export let twitter_link: string
-	export let youtube_name: string
-	export let youtube_link: string
-	export let board: string
-</script>
